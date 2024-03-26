dc5279bafb7ae6bdc39e1ef615eabc8a01ae69e6
c-chae-eun-n <ce.hue125@gmail.com>
2024-03-26 17:18:22 +0900

    Update : attack 메서드 (Zombie 공격)

--- zombie/src/zombie/Hero.java
+++ zombie/src/zombie/Hero.java
@@ -21,11 +21,21 @@ public class Hero extends Unit{
 	@Override
 	public void attack(Unit enemy) {
 		if(enemy instanceof Boss) {
-			Boss boss = (Boss) enemy;
+//			Boss boss = (Boss) enemy;
+//			
+//			power = random.nextInt(this.getMax()) + 5;
 			
-			power = random.nextInt(super.getMax()) + 5;
-			
+		}else {
+			power = random.nextInt(this.getMax()) + 5;
+			System.out.printf("히어로 %d의 공격력으로 공격!!\n", power);
+			int curHp = enemy.getHp();
+			enemy.setHp(curHp - power);
+			if(enemy.getHp() <= 0)
+				enemy.setHp(0);
+			System.out.println("좀비 현재 체력 : " + enemy);
 		}
 		
 	}
+	
+	
 }
