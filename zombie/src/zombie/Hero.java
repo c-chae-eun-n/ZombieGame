b28b78c65dd6a938f549ba41c477835fa1730637
c-chae-eun-n <ce.hue125@gmail.com>
2024-03-26 15:31:03 +0900

    Add : Hero constructor

--- zombie/src/zombie/Hero.java
+++ zombie/src/zombie/Hero.java
@@ -1,5 +1,18 @@
 package zombie;
 
-public class Hero {
+public class Hero extends Unit{
+	private int power;
+	private int potion;
 	
+
+	public Hero(int max, int hp, int position, int potion) {
+		super(max, hp, position);
+		this.potion = potion;
+	}
+	
+	@Override
+	public void attack(Unit enemy) {
+		
+		
+	}
 }
