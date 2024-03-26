2233c3b91705b12d8d81d48a842ab9f6a9359c7f
c-chae-eun-n <ce.hue125@gmail.com>
2024-03-26 16:35:04 +0900

    Add : potion getter & setter

--- zombie/src/zombie/Hero.java
+++ zombie/src/zombie/Hero.java
@@ -5,14 +5,27 @@ public class Hero extends Unit{
 	private int potion;
 	
 
-	public Hero(int max, int hp, int position, int potion) {
-		super(max, hp, position);
+	public Hero(int position, int hp, int max, int potion) {
+		super(position, hp, max);
+		this.potion = potion;
+	}
+	
+	public int getPotion() {
+		return this.potion;
+	}
+	
+	public void setPotion(int potion) {
 		this.potion = potion;
 	}
 	
 	@Override
 	public void attack(Unit enemy) {
-		
+		if(enemy instanceof Boss) {
+			Boss boss = (Boss) enemy;
+			
+			power = random.nextInt(super.getMax()) + 5;
+			
+		}
 		
 	}
 }
