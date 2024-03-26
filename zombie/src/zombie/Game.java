4f72e691c9df240f412fa36115e43730994c8dc8
c-chae-eun-n <ce.hue125@gmail.com>
2024-03-26 16:24:37 +0900

    Add : variable & constructor

--- zombie/src/zombie/Game.java
+++ zombie/src/zombie/Game.java
@@ -1,9 +1,22 @@
 package zombie;
 
-public class Game {
+import java.util.Scanner;
 
+public class Game {
+	
+	private Scanner scan = new Scanner(System.in);
+	
+	private int pos;
+	private boolean isExit;
+	private Hero hero;
+	private Zombie zombie;
+	private Boss boss;
+	
 	private Game() {
-		
+		pos = 1;
+		hero = new Hero(1, 100, 20, 2);
+		zombie = new Zombie(5, 50, 10);
+		boss = new Boss(10, 70, 30, 150);
 	}
 	
 	private static Game instance = new Game();
@@ -12,6 +25,6 @@ public class Game {
 	}
 	
 	public void run() {
-		
+
 	}
 }
