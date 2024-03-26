c4ff476004971ae736365adb30d31ea6b13de389
c-chae-eun-n <ce.hue125@gmail.com>
2024-03-26 15:40:33 +0900

    Add : secondHp getter & setter, Update variable name(shield -> secondHp)

--- zombie/src/zombie/Boss.java
+++ zombie/src/zombie/Boss.java
@@ -1,11 +1,19 @@
 package zombie;
 
 public class Boss extends Zombie {
-	private int shield;
+	private int secondHp;
 
-	public Boss(int max, int hp, int position, int shield) {
-		super(max, hp, position);
-		this.shield = shield;
+	public Boss(int max, int firstHp, int position, int secondHp) {
+		super(max, firstHp, position);
+		this.setSecondHp(secondHp);
+	}
+
+	public int getSecondHp() {
+		return this.secondHp;
+	}
+
+	public void setSecondHp(int secondHp) {
+		this.secondHp = secondHp;
 	}
 	
 }
