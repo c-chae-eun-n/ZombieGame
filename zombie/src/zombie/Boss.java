84c7abece75df918a42148470578bf62ce1856a1
c-chae-eun-n <ce.hue125@gmail.com>
2024-03-26 15:33:36 +0900

    Add : Boss constructor

--- zombie/src/zombie/Boss.java
+++ zombie/src/zombie/Boss.java
@@ -1,5 +1,11 @@
 package zombie;
 
-public class Boss {
+public class Boss extends Zombie {
+	private int shield;
+
+	public Boss(int max, int hp, int position, int shield) {
+		super(max, hp, position);
+		this.shield = shield;
+	}
 	
 }
